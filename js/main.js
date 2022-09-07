907ff0f38a7c1772ac1cc67b28b107ea746060c9
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-07 13:00:55 +0300

    adding tabs

--- js/main.js
+++ js/main.js
@@ -39,7 +39,50 @@ const modals = () => {
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 60000);
+  //showModalByTime('.popup', 60000);
 };
 
-modals()
+// Tabs
+
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach(item => {
+      item.style.display = "none";
+    });
+
+    tab.forEach(item => {
+      item.classList.remove(activeClass);
+    })
+  }
+
+  function showTabContent(i = 0) {
+    content[i].style.display = "block";
+    tab[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target &&
+      (target.classList.contains(tabSelector.replace(/\./, '')) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+
+modals();
+tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
