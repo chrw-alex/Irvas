a991813603d29a9dab20ecb919ef34460a6bed8c
Aleksandra Chervonets <chervonets.as@gmail.com>
2022-09-10 14:09:32 +0300

    adding timer

--- js/main.js
+++ js/main.js
@@ -227,6 +227,69 @@ const changeModalState = (state) => {
   bindActionToElems('change', windowProfile, 'profile');
 };
 
+// Timer
+
+let deadline = '2022-12-11';
+
+const timer = (id, deadline) => {
+
+  const addZero = (num) => {
+    if (num <= 9) {
+      return '0' + num;
+    } else {
+      return num;
+    }
+  };
+
+
+  const getTimeRemaining = (endtime) => {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          seconds = Math.floor((t/1000) % 60),
+          minutes = Math.floor((t/1000/60) % 60),
+          hours = Math.floor((t/(1000 * 60 * 60)) % 24),
+          days = Math.floor((t/(1000 * 60 * 60 * 24)));
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  };
+
+  const setClock = (selector, endtime) => {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.textContent = addZero(t.days);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        days.textContent = '00';
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+
+        clearInterval(timeInterval);
+      }
+    }
+  };
+
+  setClock(id, deadline);
+};
+
 
 changeModalState(modalState);
 modals();
@@ -234,3 +297,4 @@ tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
 forms(modalState);
+timer('.container1', deadline);
